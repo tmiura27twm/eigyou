a7bb51800f3fd9f8266f54599d5890aa095ade93
tmiura27twm <t.miura27.twm@gmail.com>
2026-07-03 10:10:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 営業ツール ラッパー Service Worker（インストール要件用・最小）
-const CACHE = 'eigyo-v1';
+const CACHE = 'eigyo-v2';
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json']).catch(() => {})));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './tanto.html', './manifest.json']).catch(() => {})));
 });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e => {
